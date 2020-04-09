16d94f09364ba2a849a3e93a7de89e5a8f26e660
adriaanbd <adriaanbd@gmail.com>
2020-04-09 14:13:18 -0500

    adding delete item icon

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,10 @@
 import React, {useState} from 'react';
-import {CheckBox, ListItem, Button} from 'react-native-elements';
+import {CheckBox, ListItem, Icon} from 'react-native-elements';
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  // custome styling
+});
 
 const Todo = props => {
   const {idx, todo} = props;
@@ -9,11 +14,25 @@ const Todo = props => {
     setDone(prevState => !prevState);
   };
 
+  const handleRemove = () => {
+    // remove logic
+  };
+
+  const handleItemPress = () => {
+    // highlight list item
+  };
+
   return (
     <ListItem
+      containerSyling={styles.container}
       key={idx}
       title={todo}
+      pad={0}
       leftElement={<CheckBox checked={done} onPress={handleCheck} />}
+      rightElement={
+        <Icon name="delete" onPress={handleRemove} color="skyblue" />
+      }
+      onPress={handleItemPress}
       bottomDivider
     />
   );
