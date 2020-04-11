596050686b1bb6f91b4cb3187d2e817e8f59a86c
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-04-11 10:24:14 +0800

    Transfer stuff to TodolistScreen

--- src/App.js
+++ src/App.js
@@ -4,30 +4,13 @@
  */
 
 import React from 'react';
-import {View, StyleSheet, ScrollView} from 'react-native';
-import Heading from './components/Heading';
-import InputField from './components/InputField';
 import TodoListScreen from './screens/TodoListScreen';
 import CombinedProvider from './context/combinedProvider';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-    paddingHorizontal: 24,
-  },
-});
 
 const App = () => {
   return (
     <CombinedProvider>
-      <View style={styles.container}>
-        <Heading />
-        <InputField />
-        <ScrollView keyboardShouldPersistTaps="always">
-          <TodoListScreen />
-        </ScrollView>
-      </View>
+      <TodoListScreen />
     </CombinedProvider>
   );
 };
--- src/screens/TodoListScreen.js
+++ src/screens/TodoListScreen.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import {ScrollView, View, StyleSheet} from 'react-native';
 import TodoList from '../components/TodoList';
+import Heading from '../components/Heading';
+import InputField from '../components/InputField';
 
-const TodoListScreen = () => <TodoList />;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    paddingHorizontal: 24,
+  },
+});
+
+const TodoListScreen = () => (
+  <View style={styles.container}>
+    <ScrollView>
+      <Heading />
+      <InputField />
+      <TodoList />
+    </ScrollView>
+  </View>
+);
 
 export default TodoListScreen;
